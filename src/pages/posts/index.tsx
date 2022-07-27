7865e20458a859b7436a4908e2adbc5c0dc2720e
MatheusGorga <matheusgobar@gmail.com>
2022-07-27 03:26:08 -0300

    prismic - listando post

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -3,9 +3,20 @@ import styles from './styles.module.scss'
 import { GetStaticProps } from "next";
 import { getPrismicClient } from '../../services/prismic';
 import * as Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom'
+
+type post = {
+  slug: string,
+  title: string,
+  excerpt: string,
+  updatedAt: string,
+}
+interface PostProps {
+  posts: post[]
+}
 
 
-export default function Posts() {
+export default function Posts({ posts }: PostProps) {
   return (
     <>
       <Head>
@@ -13,21 +24,18 @@ export default function Posts() {
       </Head>
       <main className={styles.container}>
         <div className={styles.posts}>
-          {[1, 2, 3].map(() => {
-            return (
-              <>
-                <a href="#" key={Math.random()} >
-                  <time>12 de março de 2021</time>
-                  <strong>
-                    Creating a Monorepo with Lerna & Yarn Workspaces
-                  </strong>
-                  <p>
-                    In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.
-                  </p>
-                </a>
-              </>
-            )
-          })}
+          {posts.map((post) => (
+            <a href="#" key={post.slug} >
+              <time>{post.updatedAt}</time>
+              <strong>
+                {post.title}
+              </strong>
+              <p>
+                {post.excerpt}
+              </p>
+            </a>
+          )
+          )}
         </div>
       </main>
     </>
@@ -36,11 +44,24 @@ export default function Posts() {
 
 export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient()
-  const posts = await prismic.getByType("post", {
+  const postsPrismic = await prismic.getByType("post", {
     pageSize: 100,
   });
 
-  console.log(posts)
+  const posts = postsPrismic.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-Br', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
+  })
+
+
 
   return {
     props: {
